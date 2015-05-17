5bedebea5f5d2d4a44bdab790aa3ccff97ca411c
Ian Dunn <ian@iandunn.name>
2015-05-17 01:07:40 -0700

    Convert to standard module pattern.
    This'll make it easier to learn since most examples use this pattern. Once I understand everything more, I can re-evaluate.

--- javascript/intent-driven-interface.js
+++ javascript/intent-driven-interface.js
@@ -1,81 +1,70 @@
-var IntentDrivenInterface = ( function( $ ) {
+( function( $ ) {
 	'use strict';
-	var options, mainContainer, searchField;
 
-	/**
-	 * Initialization that runs as soon as this file has loaded
-	 *
-	 * @param {object} initOptions
-	 */
-	function construct( initOptions ) {
-		options         = initOptions;
-		initOptions     = null;
-		mainContainer   = $( '#idi-container' );
-		searchField     = $( '#idi-search' );
+	var app = window.IntentDrivenInterface = {
+		/**
+		 * Initialization that runs as soon as this file has loaded
+		 */
+		construct : function() {
+			this.options       = idiOptions;
+			this.mainContainer = $( '#idi-container' );
+			this.searchField   = $( '#idi-search' );
+			idiOptions         = null;
 
-		try {
-			$( window ).keyup( toggleInterface );
-			mainContainer.click( toggleInterface );
-			searchField.keyup( showRelevantLinks );
-			// todo maybe make this a controller that calls toggleinteface, showrelevantlinks, etc. better than having two listeners for same event?
-		} catch ( exception ) {
-			log( exception );
-		}
-	}
-
-	/**
-	 * Reveal the interface when the user presses the shortcut
-	 *
-	 * @param {object} event
-	 */
-	function toggleInterface( event ) {
-		if ( 'keyup' === event.type ) {
-			if ( event.key === options.shortcuts.open ) {
-				mainContainer.addClass( 'idi-active' );
-				searchField.focus();
-			} else if ( event.key === options.shortcuts.close ) {
-				mainContainer.removeClass( 'idi-active' );
+			try {
+				$( window ).keyup( app.toggleInterface );
+				app.mainContainer.click( app.toggleInterface );
+				app.searchField.keyup( app.showRelevantLinks );
+				// todo maybe make this a controller that calls toggleinteface, showrelevantlinks, etc. better than having two listeners for same event?
+			} catch ( exception ) {
+				app.log( exception );
 			}
-		} else if ( 'click' === event.type ) {
-			if ( 'notification-dialog-background' === event.target.className || 'media-modal-icon' === event.target.className ) {
-				mainContainer.removeClass( 'idi-active' );
+		},
+
+		/**
+		 * Reveal the interface when the user presses the shortcut
+		 *
+		 * @param {object} event
+		 */
+		toggleInterface : function( event ) {
+			if ( 'keyup' === event.type ) {
+				if ( event.key === app.options.shortcuts.open ) {
+					app.mainContainer.addClass( 'idi-active' );
+					app.searchField.focus();
+				} else if ( event.key === app.options.shortcuts.close ) {
+					app.mainContainer.removeClass( 'idi-active' );
+				}
+			} else if ( 'click' === event.type ) {
+				if ( 'notification-dialog-background' === event.target.className || 'media-modal-icon' === event.target.className ) {
+					app.mainContainer.removeClass( 'idi-active' );
+				}
+			}
+		},
+
+		/**
+		 * Show relevant links based on the user's query
+		 */
+		showRelevantLinks : function() {
+			$( '#idi-instructions' ).addClass( 'idi-active' );
+			$( '#idi-menu' ).addClass( 'idi-active' );
+		},
+
+		/**
+		 * Log a message to the console
+		 *
+		 * @param {*} error
+		 */
+		log : function( error ) {
+			if ( ! window.console ) {
+				return;
+			}
+
+			if ( 'string' === typeof error ) {
+				console.log( 'Intent-Driven Interface: ' + error );
+			} else {
+				console.log( 'Intent-Driven Interface: ', error );
 			}
 		}
-	}
-
-	/**
-	 * Show relevant links based on the user's query
-	 *
-	 * @param {object} event
-	 */
-	function showRelevantLinks() {
-		$( '#idi-instructions' ).addClass( 'idi-active' );
-		$( '#idi-menu' ).addClass( 'idi-active' );
-	}
-
-	/**
-	 * Log a message to the console
-	 *
-	 * @param {*} error
-	 */
-	function log( error ) {
-		if ( ! window.console ) {
-			return;
-		}
-
-		if ( 'string' === typeof error ) {
-			console.log( 'Intent-Driven Interface: ' + error );
-		} else {
-			console.log( 'Intent-Driven Interface: ', error );
-		}
-	}
-
-	/*
-	 * Reveal public methods
-	 */
-	return {
-		construct : construct,
-		log       : log
 	};
 } )( jQuery );
 
