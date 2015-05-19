0f9bee8398a1028b320a353fd72ffc261ff15e6e
Ian Dunn <ian@iandunn.name>
2015-05-18 22:53:32 -0700

    Internationalize views/requirements-error.php

--- views/requirements-error.php
+++ views/requirements-error.php
@@ -1,18 +1,24 @@
 <?php defined( 'WPINC' ) or die; ?>
-<?php // todo finish updating text and i18n ?>
 
 <div class="error">
 	<p><?php _e( "Intent Driven Interface error: Your environment doesn't meet all of the system requirements listed below.", 'intent-driven-interface' ); ?></p>
 
 	<ul class="ul-disc">
-		<li><strong>PHP <?php echo IDI_REQUIRED_PHP_VERSION; ?>+</strong>
-			<em>(<?php printf( __( "You're running version %s", 'intent-driven-interface' ), PHP_VERSION ); ?>)</em>
+		<li>
+			<strong><?php printf( __( 'PHP %s+', 'intent-driven-interface' ), esc_html( IDI_REQUIRED_PHP_VERSION ) ); ?></strong>
+			<em><?php printf( __( "(You're running version %s)", 'intent-driven-interface' ), esc_html( PHP_VERSION ) ); ?></em>
 		</li>
 
-		<li><strong>WordPress <?php echo IDI_REQUIRED_WP_VERSION; ?>+</strong>
-			<em>(You're running version <?php echo esc_html( $wp_version ); ?>)</em>
+		<li>
+			<strong><?php printf( __( 'WordPress %s+', 'intent-driven-interface' ), esc_html( IDI_REQUIRED_WP_VERSION ) ); ?></strong>
+			<em><?php printf( __( "(You're running version %s)", 'intent-driven-interface' ), esc_html( $wp_version ) ); ?></em>
 		</li>
 	</ul>
 
-	<p>If you need to upgrade your version of PHP you can ask your hosting company for assistance, and if you need help upgrading WordPress you can refer to <a href="http://codex.wordpress.org/Upgrading_WordPress">the Codex</a>.</p>
+	<p>
+		<?php printf(
+			__( 'If you need to upgrade your version of PHP you can ask your hosting company for assistance, and if you need help upgrading WordPress you can refer to <a href="%s">the Codex</a>.', 'intent-driven-interface' ),
+			'http://codex.wordpress.org/Upgrading_WordPress'
+          ); ?>
+	</p>
 </div>
